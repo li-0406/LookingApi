ea09fd89af4b535223e1e71a21de177f4eae7adf
li-0406 <cr870406@gmail.com>
2024-02-20 21:41:20 +0800

    修正部屬

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,11 @@
-module.exports = {
-  // 其他配置...
-  resolve: {
-    fallback: {
-      stream: require.resolve("stream-browserify"),
-      util: require.resolve("util"),
-      path: require.resolve("path-browserify"),
-      url: require.resolve("url/"),
-    },
-  },
-};
+// module.exports = {
+//   // 其他配置...
+//   resolve: {
+//     fallback: {
+//       stream: require.resolve("stream-browserify"),
+//       util: require.resolve("util"),
+//       path: require.resolve("path-browserify"),
+//       url: require.resolve("url/"),
+//     },
+//   },
+// };
